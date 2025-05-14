d9405e7224a48bd172bfe04daa1e86100ab60aa9
KrishKarki <karkikrish926@gmail.com>
2025-05-13 20:19:42 -0500

    Added OOP examples and README for core Java practice

---
+++ 01_OOP/ClassesAndObjects.java
@@ -0,0 +1,12 @@
+public class ClassesAndObjects {
+    int number = 10;
+
+    void display() {
+        System.out.println("Number is: " + number);
+    }
+
+    public static void main(String[] args) {
+        ClassesAndObjects obj = new ClassesAndObjects();
+        obj.display();
+    }
+}
