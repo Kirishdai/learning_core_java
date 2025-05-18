15bd161a090e80234c6a759ebfe52d542296595a
KrishKarki <karkikrish926@gmail.com>
2025-05-18 16:25:55 -0500

    Added TwoDArraysDemo.java showing nested loop traversal of 2D array

---
+++ 05_Collections/TwoDArraysDemo.java
@@ -0,0 +1,20 @@
+// TwoDArraysDemo.java
+// Demonstrates creation and traversal of a 2D array
+
+public class TwoDArraysDemo {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("2D Array Elements:");
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
