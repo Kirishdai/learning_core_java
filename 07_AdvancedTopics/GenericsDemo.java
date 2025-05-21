d16726b3c70bde446ba88292c11faec3e21e8a9a
KrishKarki1 <131786876+Kirishdai@users.noreply.github.com>
2025-05-21 16:16:17 -0500

    Update GenericsDemo.java

--- 07_AdvancedTopics/GenericsDemo.java
+++ 07_AdvancedTopics/GenericsDemo.java
@@ -1,25 +1,37 @@
-class Box<T, U>{
+// GenericsDemo.java
+// Demonstrates how to use generics with multiple type parameters
+
+// Generic class with two type parameters: T and U
+class Box<T, U> {
     T item;
     U value;
 
-    public Box(T item, U value){
+    // Constructor to initialize item and value
+    public Box(T item, U value) {
         this.item = item;
         this.value = value;
     }
-    public T getItem(){
+
+    // Getter for item (type T)
+    public T getItem() {
         return item;
     }
-    public U getValue(){
+
+    // Getter for value (type U)
+    public U getValue() {
         return value;
     }
 }
+
 public class GenericsDemo {
     public static void main(String[] args) {
 
+        // Create Box objects with different type combinations
         Box<String, Double> box = new Box<>("Laptop", 42.15);
         Box<String, Integer> box2 = new Box<>("Tablet", 42);
 
-        System.out.println(box.getItem());
-        System.out.println(box2.getValue());
+        // Output the stored item and value
+        System.out.println(box.getItem());   // Output: Laptop
+        System.out.println(box2.getValue()); // Output: 42
     }
 }
